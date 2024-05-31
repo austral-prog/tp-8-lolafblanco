7da9a06945414ece8e964dc02945e1aa29fc2a4f
lolafblanco <162721872+lolafblanco@users.noreply.github.com>
2024-05-31 20:00:29 -0300

    Update sets.py

--- sets.py
+++ sets.py
@@ -1,15 +1,7 @@
 from sets_categories_data import (ALCOHOLS)
 
 def clean_ingredients(dish_name, dish_ingredients):
-    """Remove duplicates from `dish_ingredients`.
-
-    :param dish_name: str - containing the dish name.
-    :param dish_ingredients: list - dish ingredients.
-    :return: tuple - containing (dish_name, ingredient set).
-
-    This function should return a `tuple` with the name of the dish as the first item,
-    followed by the de-duped `set` of ingredients as the second item.
-    """
+   
     unicos_ingredients = set(ingredients)
     
     return (dish_name, unicos_ingredients)
